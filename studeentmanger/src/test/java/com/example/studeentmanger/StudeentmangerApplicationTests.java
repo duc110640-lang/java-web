1e4bd4d71f313291e9d60f914026f039452f29d2
duc110640-lang <duc110640@donga.edu.vn>
2026-08-24 13:35:13 +0700

    Add files via upload

---
+++ studeentmanger/src/test/java/com/example/studeentmanger/StudeentmangerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.studeentmanger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudeentmangerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
